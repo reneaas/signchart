f8557d0b2c372fe1f5b85cd242a46a21fcbf5b3a
reneaas <reneaas@uio.no>
2024-10-24 20:57:44 +0200

    update

--- examples/sign_chart_example_1.py
+++ examples/sign_chart_example_1.py
@@ -2,9 +2,10 @@ from signchart import make_sign_chart, show, savefig
 
 f = "(x**2 + 1)**2 * (x - 1)**2 * (x + 1)"
 make_sign_chart(f=f, include_factors=True)
-show()
 
 savefig(
     dirname="figures",
     fname="example_1.svg",
 )
+
+show()
